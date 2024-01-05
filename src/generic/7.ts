86cca9aba92dfeb67e157429197404fd9bc0fd5c
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-01-05 02:06:02 +0200

    done

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -10,10 +10,11 @@ export enum UserRole {
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+type RoleDescript = Record<UserRole, string>;
+const RoleDescription:RoleDescript = {
+  [UserRole.admin]: 'Admin User',
+  [UserRole.editor]: 'Editor User',
+  [UserRole.guest]: 'Guest User',
 };
 
 export {};
